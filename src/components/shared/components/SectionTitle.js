042a7f62b226f808491548af7a06af96078f873a
Rémi Martineau <remi.martineau.dev@gmail.com>
2021-09-20 20:26:09 +0200

    section titles are now responsive

--- src/components/shared/components/SectionTitle.js
+++ src/components/shared/components/SectionTitle.js
@@ -11,15 +11,31 @@ const Title = styled.h2`
 
 const Outline = styled.h1`
     -webkit-text-stroke: 1px var(--gray);
-    font-size: 10rem;
     font-weight: var(--fw-xl);
     letter-spacing: 0.8px;
     color: transparent;
     position: absolute;
-    top: -2rem;
     left: 0;
+    top: -2rem;
     margin: 0;
     z-index: -1;
+    
+    @media screen and (max-width: 550px){
+        display: none;
+    }
+
+    @media screen and (min-width: 550px){
+        font-size: 4.5rem;
+    }
+
+
+    @media screen and (min-width:768px){
+        font-size: 5.75rem;
+    }
+
+    @media screen and (min-width: 1250px){
+        font-size: 10rem;
+    }
 `
 
 export default function SectionTitle({title, outline}) {
